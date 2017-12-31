488e520bdf2cd96d94f1bc276344c585612ab055
rajnishcoder <getwere@gmail.com>
2017-12-31 11:18:21 +0530

    change in error

--- index.py
+++ index.py
@@ -11,8 +11,9 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
+    output = r.recognize_google(audio)
+    print(output)
 except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+    print("Sorry I did not understand! Can you say it again?")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
